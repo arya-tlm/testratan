6fe7b9896530986638e6d09226e4e3539d6da550
arya-tlm <arya.nimje@tlminsidesales.com>
2025-07-25 14:40:53 +0530

    testing

--- test_github_2.py
+++ test_github_2.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("This is 2nd file")
+    print("This is 2nd file ratan")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
