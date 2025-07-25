d99851047836648c770dd974f01a88d818ae5fe0
arya-tlm <arya.nimje@tlminsidesales.com>
2025-07-25 15:41:18 +0530

    lets see if file change

--- test_github_2.py
+++ test_github_2.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("hogya bhai tension mt le 1")
+    print("ratan bhai mera bhi hgya hogya bhai tension mt le 1")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
--- test_github_3.py
+++ test_github_3.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("mera bhi hogya bhai 1")
+    print("ratan bhai mera bhi hogya bhai 1")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
