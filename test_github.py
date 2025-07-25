46e1ad6fe47058ce6c05dff18aad0c19aad6cd60
arya-tlm <arya.nimje@tlminsidesales.com>
2025-07-25 14:45:42 +0530

    testing

--- test_github.py
+++ test_github.py
@@ -1,8 +1,8 @@
 import json
 
 def lambda_handler(event, context):
-    print("Hello World! hi ratan bhaiya")
+    print("Testing auto-deployment from PyCharm!")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Auto-deploy test - success!')
     }
